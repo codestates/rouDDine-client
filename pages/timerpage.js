85dc6bba0ac681baf259af74d20ffb7020d06d01
z1Hoon <ddungbar2@gmail.com>
2021-06-28 16:56:16 +0900

    timer page layout

---
+++ pages/timerpage.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+let Body = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  flex-direction: column;
+  padding: 7%;
+  /* justify-content: space-between; */
+`;
+
+let Info = styled.div`
+  display: flex;
+  justify-content: space-around;
+  div {
+    font-size: 3rem;
+  }
+`;
+
+let Time = styled.div`
+  align-self: center;
+  font-size: 15rem;
+  margin: 5%;
+`;
+
+export default function timerpage() {
+  return (
+    <Body>
+      <Info>
+        <div>루틴이름</div>
+        <div>현재운동</div>
+        <div>현재세트</div>
+      </Info>
+      <Time>00:00</Time>
+    </Body>
+  );
+}
