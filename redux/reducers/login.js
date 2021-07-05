a6f7d59a46536e42e539259d9e6c9eae35fcf632
kwd8905 <cloud.road89@gmail.com>
2021-07-05 10:52:02 +0900

    ✏️ 충돌제거 2

--- redux/reducers/login.js
+++ redux/reducers/login.js
@@ -1,32 +1,45 @@
-export const LOGIN = 'LOGIN';
+export const initialState = {
+  userId: null,
+}; // 처음 state값으로 count 0을 주었다. state값은 객체, 배열로도 사용할 수 있다.
+
+export const LOGIN_USER1 = 'LOGIN_USER1';
+export const LOGIN_USER2 = 'LOGIN_USER2';
+export const LOGIN_USER3 = 'LOGIN_USER3';
 export const LOGOUT = 'LOGOUT';
 
-const initialState = {
-  isLogin: false,
-};
-export default function reducer(state = initialState, action) {
-  // 리듀서
-  switch (action.type) {
-    case LOGIN:
-      return {
-        ...state,
-        isLogin: true,
-      };
-    case LOGOUT:
-      return {
-        ...state,
-        isLogin: false,
-      };
-    default:
-      return state;
-  }
-}
+export const loginUser1Action = () => ({ // 액션 생성 함수
+  type : LOGIN_USER1
+});
+export const loginUser2Action = () => ({ // 액션 생성 함수
+  type : LOGIN_USER2
+});
+export const loginUser3Action = () => ({ // 액션 생성 함수
+  type : LOGIN_USER3
+});
+export const logoutAction = () => ({ // 액션 생성 함수
+  type : LOGOUT
+});
 
-export const loginAction = () => ({
-  // 액션 생성 함수
-  type: LOGIN,
-});
-export const logoutAction = () => ({
-  // 액션 생성 함수
-  type: LOGOUT,
-});
+const reducer = (state=initialState, action) => { // 리듀서
+    switch (action.type) { 
+        case LOGIN_USER1:
+          return state = {
+            ...state,
+            userId: 1,
+          }
+        case LOGIN_USER2:
+          return state = {
+            ...state,
+            userId: 2,
+          }
+        case LOGIN_USER3:
+          return state = {
+            ...state,
+            userId: 3,
+          }        
+        default:
+          return initialState;
+    }
+};
+
+export default reducer; 
