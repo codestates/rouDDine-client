a699eb559ee3402dc78ff9068c77c251c4b5d52c
z1Hoon <ddungbar2@gmail.com>
2021-07-13 10:52:27 +0900

    🎨  모달 진행중

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,19 @@
 import '../styles/globals.css';
-import Nav from '../src/components/Nav/Nav';
 import 'semantic-ui-css/semantic.min.css';
 import wrapper from '../redux/store/index';
 import HeadInfo from '../src/components/HeadInfo/HeadInfo';
+import Nav from '../src/components/Nav/Nav';
 import Head from 'next/head';
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
-      <HeadInfo />
       <Head>
         <meta name='viewport' content='viewport-fit=cover' />
       </Head>
       <Nav />
-      <Component {...pageProps} />
+      <HeadInfo />
+      <Component {...pageProps} />;
     </>
   );
 };
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,59 +1,85 @@
 import styled from 'styled-components';
 import Link from 'next/link';
 import Cookies from 'js-cookie';
-
-const NavContainer = styled.div`
-  background-color: whitesmoke;
-  display: flex;
-  justify-content: space-between;
-  top: 0;
-  height: 60px;
-  width: 100vw;
-  padding: 10px;
-  position: fixed;
-  z-index: 2;
-  /* box-shadow: 0 0 3px 1px rgb(0 0 0 / 10%); */
-  > div {
-    display: flex;
-    margin: 0px 20px 0px 0px;
-    justify-content: space-between;
-  }
-  .link {
-    color: white;
-    cursor: pointer;
-    padding: 10px;
-  }
-`;
+import SignUp from '../login&signup/signup';
+import Login from '../login&signup/login';
+import { useState } from 'react';
 
 export default function Nav() {
   const accessToken = Cookies.get('accessToken');
-
+  const [modalLogin, setModalLogin] = useState(false);
+  const [modalSignup, setModalSignup] = useState(false);
   return (
     <>
       <NavContainer>
         <Link href='/'>
           <div className='link'>로고</div>
         </Link>
-
+        {modalLogin || modalSignup ? <Overlay_modal /> : null}
+        {modalLogin && <Login />}
+        {modalSignup && <SignUp />}
         <ButtonContainer>
           {accessToken ? (
             <Link href='/Mypage'>
               <div className='link'>마이페이지</div>
             </Link>
           ) : (
-            <Link href='/login'>
-              <div className='link'>로그인</div>
-            </Link>
+            <div className='link' onClick={() => setModalLogin(true)}>
+              로그인
+            </div>
           )}
           {accessToken ? (
-            <div onClick={() => Cookies.remove('accessToken')}>로그아웃</div>
+            <div className='link' onClick={() => Cookies.remove('accessToken')}>
+              로그아웃
+            </div>
           ) : (
-            <Link href='/signup'>
-              <div className='link'>회원가입</div>
-            </Link>
+            <div className='link' onClick={() => setModalSignup(true)}>
+              회원가입
+            </div>
           )}
         </ButtonContainer>
       </NavContainer>
     </>
   );
 }
+
+const NavContainer = styled.div`
+  position: absolute;
+  background-color: white;
+  display: flex;
+  justify-content: space-between;
+  height: 60px;
+  width: 100vw;
+  padding: 10px;
+  position: fixed;
+  z-index: 103;
+  box-shadow: 0 0 3px 1px rgb(0 0 0 / 10%);
+  > div {
+    display: flex;
+    margin: 0px 20px 0px 0px;
+    justify-content: space-between;
+  }
+  .link {
+    color: black;
+    cursor: pointer;
+    padding: 10px;
+  }
+  .modal {
+    position: fixed;
+  }
+`;
+const ButtonContainer = styled.div`
+  display: flex;
+`;
+
+const Overlay_modal = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  background: rgba(0, 0, 0, 0.495);
+  z-index: 99;
+`;
